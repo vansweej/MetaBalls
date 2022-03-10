be6268fd0cb6ee6473abfbae810c7a78361516f3
Vitor Menezes <vmenezes599@gmail.com>
2022-03-10 16:32:03 +0100

    Added a real criterion code on the test.

--- cuda/benchmark/src/criterion_test.cpp
+++ cuda/benchmark/src/criterion_test.cpp
@@ -1,5 +1,18 @@
 #include <criterion/criterion.hpp>
 
-int main() {
-  return 0;
+BENCHMARK(Simple_test)
+{
+  SETUP_BENCHMARK(
+    const auto size = 100;
+    std::vector<int> vec(size, 0); // vector of size 100
+  )
+ 
+  // Code to be benchmarked
+  // merge_sort(vec.begin(), vec.end(), std::less<int>(), size);
+  
+  TEARDOWN_BENCHMARK(
+    vec.clear();
+  )
 }
+
+CRITERION_BENCHMARK_MAIN()
